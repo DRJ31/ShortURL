73fa2ee3979d9e73fb1054fd021f7acf17bcb9e5
dengrenjie <dengrenjie31@gmail.com>
2021-03-14 15:15:00 +0800

    Finish base64 conversion

--- src/funcs/BaseConvert.h
+++ src/funcs/BaseConvert.h
@@ -4,12 +4,17 @@
 #include <string>
 #include <stack>
 
+#define B64TABLE "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
 namespace shorturl {
 namespace funcs {
 template <typename T>
 class BaseConvert {
 public:
     static std::string decToBase62(T num);
+    static std::string decToBinary(T num, int bits);
+    static std::string base64Encode(std::string str);
+    static std::string base64Decode(std::string str);
 };
 
 template <typename T>
@@ -40,9 +45,126 @@ std::string BaseConvert<T>::decToBase62(T num) {
     }
     return result;
 }
-}
+
+template <typename T>
+std::string BaseConvert<T>::decToBinary(T num, int bits) {
+    int remainder, count = 0;
+    std::string result;
+    std::stack<char> resultStack;
+
+    while (num > 0) {
+        remainder = num % 2 + '0';
+        resultStack.push(remainder);
+        count++;
+        num /= 2;
+    }
+
+    // Fill empty bits
+    if (count < bits) {
+        for (int i = 0; i < bits - count; i++) {
+            result += '0';
+        }
+    }
+
+    while (!resultStack.empty()) {
+        result += resultStack.top();
+        resultStack.pop();
+    }
+    return result;
 }
 
+template <typename T>
+std::string BaseConvert<T>::base64Encode(std::string str) {
+    std::string binaryStr, result;
+    std::string::iterator it;
+    int count = 0, index = 0, tmp;
+
+    // Construct binary string
+    it = str.begin();
+    while (it != str.end()) {
+        binaryStr += BaseConvert<char>::decToBinary(*it, 8);
+        it++;
+    }
+
+    // Convert binary string to Base64 string
+    it = binaryStr.begin();
+    while (it != binaryStr.end()) {
+        if (count < 6) {
+            tmp = *it - '0';
+            for (int i = 0; i < 5 - count; i++) {
+                tmp *= 2;
+            }
+            index += tmp;
+            count++;
+            it++;
+        }
+        else {
+            result += B64TABLE[index];
+            count = 0;
+            index = 0;
+        }
+    }
+
+    if (index > 0 || count > 0) {
+        result += B64TABLE[index];
+        index = 0;
+        count = 0;
+    }
+
+    // Check the tail of Base64
+    if (binaryStr.size() % 24 >= 12) {
+        result += "=";
+    }
+    else if (binaryStr.size() % 24 >= 6) {
+        result += "==";
+    }
+
+    return result;
+}
+
+template <typename T>
+std::string BaseConvert<T>::base64Decode(std::string str) {
+    std::string binaryStr, result;
+    std::string::iterator it;
+    int count = 0, ascii = 0, tmp, index;
+
+    // Get binary string of Base64 string
+    it = str.begin();
+    while (it != str.end() && *it != '=') {
+        index = 0;
+        for (char ch : B64TABLE) {
+            if (ch == *it) break;
+            index++;
+        }
+        binaryStr += BaseConvert<int>::decToBinary(index, 6);
+        it++;
+    }
+
+    // Convert binary string to original string
+    it = binaryStr.begin();
+    while (it != binaryStr.end()) {
+        if (count < 8) {
+            tmp = *it - '0';
+            for (int i = 0; i < 7 - count; i++) {
+                tmp *= 2;
+            }
+            ascii += tmp;
+            count++;
+            it++;
+        }
+        else {
+            result += static_cast<char>(ascii);
+            ascii = 0;
+            count = 0;
+        }
+    }
+    if (ascii > 0) {
+        result += static_cast<char>(ascii);
+    }
+    return result;
+}
+}
+}
 
 
 #endif //SHORTURL_FUNCS_BASECONVERT_H
--- src/tests/BaseConvertUnitTest.cc
+++ src/tests/BaseConvertUnitTest.cc
@@ -10,6 +10,36 @@ TEST(BaseConvertTest, Base62) {
     EXPECT_STREQ(str.data(), "1LkSxR");
 }
 
+TEST(BaseConvertTest, Binary) {
+    std::string str = BaseConvert<long>::decToBinary(7, 8);
+    ASSERT_TRUE(str.compare(""));
+    EXPECT_STREQ(str.data(), "00000111");
+}
+
+TEST(BaseConvertTest, Base64Encode) {
+    std::string str1 = BaseConvert<std::string>::base64Encode("1234");
+    std::string str2 = BaseConvert<std::string>::base64Encode("123");
+    std::string str3 = BaseConvert<std::string>::base64Encode("12");
+    ASSERT_TRUE(str1.compare(""));
+    ASSERT_TRUE(str2.compare(""));
+    ASSERT_TRUE(str3.compare(""));
+    EXPECT_STREQ(str1.data(), "MTIzNA==");
+    EXPECT_STREQ(str2.data(), "MTIz");
+    EXPECT_STREQ(str3.data(), "MTI=");
+}
+
+TEST(BaseConvertTest, Base64Decode) {
+    std::string str1 = BaseConvert<std::string>::base64Decode("MTIzNA==");
+    std::string str2 = BaseConvert<std::string>::base64Decode("MTIz");
+    std::string str3 = BaseConvert<std::string>::base64Decode("MTI=");
+    ASSERT_TRUE(str1.compare(""));
+    ASSERT_TRUE(str2.compare(""));
+    ASSERT_TRUE(str3.compare(""));
+    EXPECT_STREQ(str1.data(), "1234");
+    EXPECT_STREQ(str2.data(), "123");
+    EXPECT_STREQ(str3.data(), "12");
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
 
